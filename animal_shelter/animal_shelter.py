1c597704faca79971bdced27fc87d3aef180442a
ShathaAlrayyani <shatha.alrayyani223@gmail.com>
2022-06-29 03:23:08 +0300

    code challenge 12 not finished

--- animal_shelter/animal_shelter.py
+++ animal_shelter/animal_shelter.py
@@ -1,69 +1,39 @@
-class IsEmpty(Exception):
-    pass
+from stack_and_queue.stack_and_queue import Queue
+
+class AnimalShelter:
+    def __init__(self, front=None, rear=None):
+        self.front = front
+        self.rear = rear
+
+    def enqueue(self, value):
+        """
+        adds a new animal with that value to the back of the queue with an O(1) Time performance.
+        """
+
+        animal = Queue()
+        if not self.rear:
+            self.front = animal
+            self.rear = animal
+        else:
+            self.rear.next = animal
+            self.rear = animal
 
 
-class AnimalShelter(object):
-    def __init__(self):
-        self.dogs = None
-        self.cats = None
-        self.order = 0
+    def dequeue(self, pref):
+        """
+        Returns: the pref animal if it was the front but if the front not equal
+        to pref it will remove it and then re add it to the queue
+        then keeps doing it until it matches the preference
+        """
+        if self.rear is not None:
+            if pref == 'dog' or pref == 'cat':
 
-    """
-    @param {string} name
-    @param {string} type Animal is dog or cat
-    @return nothing
-    """
-
-    def enqueue(self, name, type):
-        if type == 'dog':
-            self.dogs = name
-            self.order += 1
-        elif type == 'cat':
-            self.cats = name
-            self.order += 1
-
-    # return a string
-    def dequeue(self):
-        cats = []
-        dogs = []
-        if not self.dogs and not self.cats:
-            raise IsEmpty('the list is empty')
-        if self.cats:
-            removed = self.cats[0]
-            self.cats = self.cats[1]
-            removed.next = None
-            return removed.value
-        if self.dogs:
-            removed = self.dogs
-            self.dogs = self.dogs
-            removed.next = None
-            return removed.value
-
-    # def __str__(self):
-    #     current = self.cats
-    #     current2 = self.dogs
-    #     item1 = ''
-    #     item2 = ''
-    #     if current is None and current2 is None:
-    #         raise IsEmpty('lists are empty')
-    #     while current:
-    #         item1 += f"{current} \n"
-    #         current = current.next
-    #     while current2:
-    #         item2 += f"{current2} \n"
-    #         current2 = current2.next
-    #
-    #     return item1, item2
-
-
-if __name__ == '__main__':
-    stack = AnimalShelter()
-    stack.enqueue('candy', 'cat')
-    stack.enqueue('rocky', 'dog')
-    # stack.enqueue(5)
-    # stack.enqueue(7)
-    # print(stack.__str__())
-    # print(stack.dequeue())
-    print('after dequeue method')
-    stack.dequeue()
-
+                while self.front.data != pref:
+                    selected_animal = self.front
+                    self.front = self.front.next
+                    selected_animal.next = None
+                return selected_animal.data
+            else:
+                return None
+        else:
+            raise Exception('queue is empty')
--- test/test_animal_shelter.py
+++ test/test_animal_shelter.py
@@ -1,2 +1,29 @@
 import pytest
+from animal_shelter.animal_shelter import *
+
+
+def test_enqueue_single_cat():
+    """
+    tset enqueue single cat and check the dequeue based on pref
+    """
+    shelter = AnimalShelter()
+    cat = 'sherry'
+    shelter.enqueue(cat)
+    actual = shelter.dequeue('cat')
+    expected = cat
+    assert actual == expected
+
+
+def test_enqueue_single_dog():
+    """
+    tset enqueue single dog and check the dequeue based on pref
+    """
+    shelter = AnimalShelter()
+    dog = 'Alex'
+    shelter.enqueue(dog)
+    actual = shelter.dequeue('dog')
+    expected = dog
+    assert actual == expected
+
+
 
