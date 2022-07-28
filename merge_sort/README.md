0ad78e4c52867f221e9d8ccc35a91db5d0855b75
ShathaAlrayyani <shatha.alrayyani223@gmail.com>
2022-07-28 03:41:46 +0300

    Code Challenge 27

---
+++ merge_sort/README.md
@@ -0,0 +1,25 @@
+# Challenge Summary
+
+<!-- Description of the challenge -->
+Write a function that takes an array of integers and return a sorted array by tracing the algorithm using the pseudocode
+provided.
+
+## Whiteboard Process
+![Merge Sort](merge_sort.jpg)
+## Approach & Efficiency
+
+- Time complexity: O(nlog(n))
+- Space complexity:O(n) 
+
+## Solution
+
+First you need to call the function then give it an array as an argument as below:
+
+        merge_sort([5, 4, 7, 2, 8, 3])
+
+        OutPut: [2, 3, 4, 5, 7, 8]
+
+
+## Tests :
+
+pytest tests/test_merge_sort.py
---
+++ test/test_merge_sort.py
@@ -0,0 +1,30 @@
+import pytest
+from merge_sort.merge_sort import *
+
+def test_empty_array():
+    actual = merge_sort([])
+    assert actual == []
+
+def test_positive_numbers():
+    actual = merge_sort([5, 7, 9, 6, 1, 7, 4, 6])
+    assert actual == [1, 4, 5, 6, 6, 7, 7, 9]
+
+def test_negative_numbers():
+    actual = merge_sort([-3, -2, -9, -7])
+    assert actual == [-9, -7, -3, -2]
+
+def test_mixed_numbers():
+    actual = merge_sort([0, 5, 2, 7, 10, -2])
+    assert actual == [-2, 0, 2, 5, 7, 10]
+
+def test_reverse_sorted():
+    actual = merge_sort([20, 18, 12, 8, 5, -2])
+    assert actual == [-2, 5, 8, 12, 18, 20]
+
+def test_few_uniques():
+    actual = merge_sort([5, 12, 7, 5, 5, 7])
+    assert actual == [5, 5, 5, 7, 7, 12]
+
+def test_nearly_sorted():
+    actual = merge_sort([2, 3, 5, 7, 13, 11])
+    assert actual == [2, 3, 5, 7, 11, 13]
